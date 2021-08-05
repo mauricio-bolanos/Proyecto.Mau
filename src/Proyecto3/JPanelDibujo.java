92cdd66bd339460c9ba271c336d330090dbf6020
maubo <maubo@LAPTOP-CSVB4B9H>
2021-08-05 17:45:56 -0600

    Proyecto punto 3

---
+++ src/Proyecto3/JPanelDibujo.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto3;
+
+import javax.swing.*;
+import java.awt.*;
+/**
+ *
+ * @author maubo
+ */
+public class JPanelDibujo extends JPanel {
+    /* 
+   * Constructor del panel para dibujar
+   */
+  public JPanelDibujo(){
+    this.setBackground(Color.RED);
+  }
+  
+
+  
+  // Reimplementa paint
+  @Override
+  public void paint (Graphics g) { 
+        super.paint(g);
+            int random = (int) (Math.random()*12);
+            
+            for (int i = 1; i<= random; i= i+1) {
+            int posX = (int) (Math.random() * 600);
+            int posY = (int) (Math.random() * 600);
+            int ancho = (int) (Math.random() * 600);
+            int alto = (int) (Math.random() * 600);
+            int posXRect = (int) (Math.random() * 600);
+            int posYRect = (int) (Math.random() * 600);
+            int anchoRect = (int) (Math.random() * 600);
+            int altoRect = (int) (Math.random() * 600);
+        
+                Ovalo ov = new Ovalo(posX,posY,ancho,alto);
+
+            //OVALOS
+            g.drawOval(ov.getPosX(), ov.getPosY(),ov.getAncho(), ov.getAlto()); //dibuja un ovalo sin fondo
+            g.setColor(Color.yellow);     
+            g.fillOval(ov.getPosX(), ov.getPosY(),ov.getAncho(), ov.getAlto());  //dibuja un ovalo con fondo amarillo
+
+
+            //RECTANGULOS
+            g.setColor(Color.blue);
+            g.fillRect(posXRect,posYRect,anchoRect,altoRect);
+            g.setColor(Color.black);
+            g.drawRect(posXRect,posYRect,anchoRect,altoRect);
+            }
+        
+
+
+     }
+}
---
+++ src/Proyecto3/Tester.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Proyecto3;
+
+/**
+ *
+ * @author maubo
+ */
+public class Tester {
+     public static void main(String argv[]){
+      // Se crea una ventana con el titulo especifico
+      MiVentana miVentana = new MiVentana("VENTANA PARA GRAFICAR");
+      
+   }
+}
